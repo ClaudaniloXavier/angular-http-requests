6f20458eb56f90034799bd343a3510769565d45b
Claudanilo Xavier <claudaniloxavier@gmail.com>
2017-07-27 23:19:14 -0300

    ajuste

--- api/src/api/confBd/config.js
+++
@@ -1,1 +0,0 @@
-module.exports = "postgres://postgres:123456@localhost/frau";
--- api/src/api/middleware/error.js
+++
@@ -1,4 +0,0 @@
-exports.notFound = function (req, res) {
-    if (!res.finished)
-        res.status(404).json({"error": "Recurso não encontrado."})
-};
